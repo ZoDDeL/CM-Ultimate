f79680cc4d1d025b75b0e9dab23fb05a05d4519e
ZoDDeL <ZoDDeL@users.noreply.github.com>
2025-04-03 14:36:08 +0200

    Delete backup combat master profile.cmd

--- backup combat master profile.cmd
+++
@@ -1,9 +0,0 @@
-
-
-setlocal enabledelayedexpansion
-
-powershell -Command "$sourcePath = Join-Path -Path $env:USERPROFILE -ChildPath 'AppData\LocalLow\AlfaBravoInc\Combat Master'; if(-not(Test-Path -Path $sourcePath)) { Write-Error 'Source folder not found'; exit 1 }; $date = Get-Date -Format 'yyyy-MM-dd'; $zipName = 'CombatMaster_' + $date + '.zip'; $destPath = Join-Path -Path (Split-Path -Path $sourcePath) -ChildPath $zipName; Compress-Archive -Path $sourcePath -DestinationPath $destPath -Force; if($?) { Write-Host 'ZIP-Archive succesfull created: ' $destPath }"
-
-endlocal
-
-pause
